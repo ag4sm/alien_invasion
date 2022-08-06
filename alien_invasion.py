a8288ed0cc13787a5bcee3f236feb81476d5acce
Josh Scoville <ag4sm@outlook.com>
2022-08-06 12:33:54 -0400

    Settings file setup and ship image setup

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,14 +1,13 @@
 import sys
 import pygame
+from settings import Settings
 
 def run_game():
-    # Initialize game and create a screen object
+    # Initialize pygame, settings screen object
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-
-    # set the background color
-    bg_color = (230,230,230)
 
     #start the main game loop
     while True:
@@ -17,7 +16,7 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
         # Redraw the screen during each pass through the loop
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
         # make the most recently drawn screen visible
         pygame.display.flip()
         
---
+++ ship.py
@@ -0,0 +1,20 @@
+import pygame
+
+class Ship():
+
+    def __init__(self,screen):
+        """Initialize ship and set starting position."""
+        self.screen = screen
+
+        # load ship image
+        self.image = pygame.image.load('/images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get.rect()
+
+        # start each ship at the bottom center of the screen.
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+    def bitme(self):
+        """Draw the ship at it's current location."""
+        self.screen.bit(self.image, self.rect)
