5daf972934c08a1192fd0330d6d002f6c12be7cd
ag4sm <56977525+ag4sm@users.noreply.github.com>
2022-08-08 11:13:00 -0400

    ship settings tweaks

--- ship.py
+++ ship.py
@@ -6,7 +6,7 @@ class Ship():
         self.screen = screen
 
         # load ship image
-        self.image = pygame.image.load("/images/ship.bmp")
+        self.image = pygame.image.load("./images/ship.bmp")
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get.rect()
 
