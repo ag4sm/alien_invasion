a6ea4e875363966fb87091abc3280ed275c35d5c
ag4sm <56977525+ag4sm@users.noreply.github.com>
2022-08-09 16:19:43 -0400

    ship file bug fixed

--- ship.py
+++ ship.py
@@ -8,7 +8,7 @@ class Ship():
         # load ship image
         self.image = pygame.image.load("./images/ship.bmp")
         self.rect = self.image.get_rect()
-        self.screen_rect = screen.get.rect()
+        self.screen_rect = screen.get_rect()
 
         # start each ship at the bottom center of the screen.
         self.rect.centerx = self.screen_rect.centerx
