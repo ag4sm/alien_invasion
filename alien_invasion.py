310d1b0b10f8cc8266524cc198be391c16c87e2f
Josh Scoville <ag4sm@outlook.com>
2022-08-09 18:51:03 -0400

    cursor movement right/left functionality

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,7 @@
-import sys
 import pygame
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     # Initialize pygame, settings, screen object
@@ -15,16 +15,8 @@ def run_game():
 
     #start the main game loop
     while True:
-        # watch for keyboard and mouse events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        # Redraw the screen during each pass through the loop
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        # make the most recently drawn screen visible
-        pygame.display.flip()
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()
