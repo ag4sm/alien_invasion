20a82cc16df53585c0655331db91e69f84b72f21
Josh Scoville <ag4sm@outlook.com>
2022-08-10 20:45:57 -0400

    direction edits and refactoring

--- game_functions.py
+++ game_functions.py
@@ -7,17 +7,19 @@ def check_events(ship):
             if event.type == pygame.QUIT:
                 sys.exit()
 
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    ship.moving_left = True
+def check_keydown_events(event, ship):
+    """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            ship.moving_left = True
 
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    ship.moving_left = False
+def check_keyup_events(event, ship):
+    """Respond to key releases."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
 
 def update_screen(ai_settings, screen, ship):
     # Redraw the screen during each pass through the loop
