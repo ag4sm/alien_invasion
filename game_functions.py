2cb5bf2b52c94fc9d8d71b8571124d98060f90e4
Josh Scoville <ag4sm@outlook.com>
2022-08-10 20:52:51 -0400

    direction edits and refactoring

--- game_functions.py
+++ game_functions.py
@@ -6,13 +6,17 @@ def check_events(ship):
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                check_keydown_events(event, ship)
+            elif event.type == pygame.KEYUP:
+                check_keyup_events(event, ship)
 
 def check_keydown_events(event, ship):
     """Respond to keypresses."""
-        if event.key == pygame.K_RIGHT:
-            ship.moving_right = True
-        elif event.key == pygame.K_LEFT:
-            ship.moving_left = True
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
 
 def check_keyup_events(event, ship):
     """Respond to key releases."""
